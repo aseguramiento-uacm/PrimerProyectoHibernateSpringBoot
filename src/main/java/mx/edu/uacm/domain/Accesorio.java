0740d19b12ffe3a95d396b72e4ab63f16ccdf5df
Profesor <aseguramiento.uacm@gmail.com>
2018-11-27 14:47:29 -0600

    Se agregan cambios a github
    Se agregan cambios de la aplicación web y pruebas unitarias, la conexion de la base se quita por cuestiones de seguridad

--- src/main/java/mx/edu/uacm/domain/Accesorio.java
+++ src/main/java/mx/edu/uacm/domain/Accesorio.java
@@ -1,10 +1,53 @@
 package mx.edu.uacm.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Accesorio {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column
 	private String nombreAccesorio;
+	
 	public Accesorio(String nombreAccesorio) {
 		this.nombreAccesorio = nombreAccesorio;
 	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nombreAccesorio
+	 */
+	public String getNombreAccesorio() {
+		return nombreAccesorio;
+	}
+
+	/**
+	 * @param nombreAccesorio the nombreAccesorio to set
+	 */
+	public void setNombreAccesorio(String nombreAccesorio) {
+		this.nombreAccesorio = nombreAccesorio;
+	}
+	
+	
 
 }
--- src/test/java/mx/edu/uacm/test/domain/VehiculoTest.java
+++ src/test/java/mx/edu/uacm/test/domain/VehiculoTest.java
@@ -2,8 +2,17 @@ package mx.edu.uacm.test.domain;
 
 
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +42,18 @@ public class VehiculoTest {
 		Vehiculo vehiculo = new Vehiculo();
 		//el id es auto generado por la bd
 		//Agregando el modelo
-		vehiculo.setModelo("2019");
+		vehiculo.setModelo("2021");
+		
 		//vehiculo.setId(1);
 		
 		//Aqui se guarda el objeto vehiculo en bd
 		vehiculoRepository.save(vehiculo);
+		
+		
+		Assert.assertNotNull(vehiculo.getId());
+		
+		Assert.assertThat(vehiculo.getId(), is(not(nullValue())));
+		
 	}
 	
 	@Test
@@ -46,6 +62,30 @@ public class VehiculoTest {
 		Accesorio accesorio = 
 				new Accesorio("Aleron");
 		
+		Accesorio accesorioDos = 
+				new Accesorio("Rosario");
+		
+		//Crear mi coleccion, pero esta vacia
+		List<Accesorio> accesorios =
+				new ArrayList<Accesorio>();
+		
+		//Agregando los accesorios a mi coleccion
+		accesorios.add(accesorio);
+		accesorios.add(accesorioDos);
+		
+		Vehiculo vehiculo = new Vehiculo();
+		//vehiculo.setId(1);
+		vehiculo.setModelo("2019");
+		
+		vehiculo.setAccessorios(accesorios);
+		
+		vehiculoRepository.save(vehiculo);
+		
+		//para buscar un vehiculo en la base de datos
+		Optional<Vehiculo> vehiculoRescatado = vehiculoRepository.findById(new Long(vehiculo.getId()));
+		
+		
+		//Assert.assertTrue();
 	}
 	
 
